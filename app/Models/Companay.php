5c22be597b869b6bac60a75903c99c8c3b12b6ce
abdulwaliaryan7 <abdulwaliaryan897@gmail.com>
2025-03-16 14:52:00 +0430

    the country, company,productType, product and  khat_estation migration is added

---
+++ app/Models/Companay.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Companay extends Model
+{
+    /** @use HasFactory<\Database\Factories\CompanayFactory> */
+    use HasFactory;
+}
---
+++ app/Models/Transport_type.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transport_type extends Model
+{
+    /** @use HasFactory<\Database\Factories\TransportTypeFactory> */
+    use HasFactory;
+}
