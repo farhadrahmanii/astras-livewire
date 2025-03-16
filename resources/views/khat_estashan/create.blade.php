fb2f2aa1a68ed56ffc5b7b9e0c04bbc08a9fa0f2
abdulwaliaryan7 <abdulwaliaryan897@gmail.com>
2025-03-16 15:50:34 +0430

    the less changes

--- resources/views/khat_estashan/create.blade.php
+++ resources/views/khat_estashan/create.blade.php
@@ -1,3 +1,3 @@
-<section class="w-full">
+<x-layouts.app title="dashboard">
     @livewire('khat-estashan.create')
-</section>
+</x-layouts.app>
--- resources/views/livewire/khat-estashan/create.blade.php
+++ resources/views/livewire/khat-estashan/create.blade.php
@@ -1,4 +1,5 @@
 <div>
     {{-- The whole world belongs to you. --}}
-   <h2>this is the form for create khat estashan</h2>
+   <h2 >this is the form for create khat estashan</h2>
+  
 </div>
